424653bcc046a344063f2a9952710a288344f51c
jwang1 <sigkdd@yahoo.com>
2018-11-11 22:29:05 -0500

    adding the flipCard logic
    the app icon is not easy to set when I created icons;  seems the size restriction ...

--- matchingCards/ViewController.swift
+++ matchingCards/ViewController.swift
@@ -15,6 +15,18 @@ class ViewController: UIViewController {
     
     @IBAction func touchCard(_ sender: UIButton) {
         print("clicked \(sender.currentTitle ?? "testing")")
+        
+        flipCard(withEmoji: "👻", on: sender)
+    }
+    
+    func flipCard(withEmoji emoji: String, on button: UIButton) {
+        if button.currentTitle == emoji {
+            button.setTitle("", for: .normal)
+            button.backgroundColor = #colorLiteral(red: 1.0, green: 1.0, blue: 1.0, alpha: 1.0)
+        } else {
+            button.setTitle(emoji, for: .normal)
+            button.backgroundColor = #colorLiteral(red: 1, green: 0.5763723254, blue: 0, alpha: 1)
+        }
     }
     
     
